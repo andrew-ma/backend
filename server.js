81c4131ded06e08e1e9b4a9be6f94cfa5cc1f9db
andrew-ma <andrew-ma@users.noreply.github.com>
2021-04-30 01:15:15 -0500

    Set default port to 4000

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const router = require("./router.js");
 
 const app = new Koa();
 
+const PORT = process.env.PORT || 4000;
+
+console.log(`Running koa server on port ${PORT}...`);
 app.use(cors()).use(serve("./images")).use(router.routes());
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT);
