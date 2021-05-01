fe6776bdf27e739d85bb0043915f112c90e9efe9
andrew-ma <andrew-ma@users.noreply.github.com>
2021-05-01 00:07:46 -0500

    Display swagger UI on backend root path

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const Koa = require("koa");
 const cors = require("@koa/cors");
 const serve = require("koa-static");
 const bodyParser = require("koa-bodyparser");
+const swagger = require("swagger2");
+const { ui, validate } = require("swagger2-koa");
 const router = require("./router.js");
+
+const swaggerDocument = swagger.loadDocumentSync("api.yaml");
 
 const app = new Koa();
 
@@ -12,6 +16,9 @@ console.log(`Running koa server on port ${PORT}...`);
 app.use(bodyParser());
 app.use(cors());
 app.use(serve("./images"));
+app.use(ui(swaggerDocument, "/"));
+app.use(validate(swaggerDocument));
 app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(PORT);
